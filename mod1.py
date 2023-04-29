73adc5e061ddad5a95e2c9529b2a6af68ce27c61
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2023-04-29 18:20:04 +0900

    Add files via upload

---
+++ mod1.py
@@ -0,0 +1,29 @@
+﻿class FourCal:
+	def __init__(self, num1, num2): #생성자
+		self.first = num1
+		self.second = num2
+	def add(self):
+		self.result = self.first + self.second
+	def div(self):
+		self.result = self.first / self.second
+
+class MoreFourCal(FourCal):
+	def sub(self):
+		self.result = self.first - self.second
+	def mul(self):
+		self.result = self.first * self.second
+	def div(self): 
+		if self.second == 0 :
+			self.result = 0
+		else:
+			self.result = self.first / self.second
+
+PI = 3.141592
+
+def sum1(num1, num2):
+	result = num1 + num2
+	return result
+
+mmCal = MoreFourCal(20, 400)
+
+a = 10
